472fbee7c9ce512038b485fff60cff23e27611f9
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-07-07 18:17:50 +0200

    Initial implementation

---
+++ src/Layout.fs
@@ -0,0 +1,58 @@
+module Layout
+
+open Giraffe.GiraffeViewEngine
+open System.IO
+
+let layout dir (content: XmlNode list) =
+    html [_class "has-navbar-fixed-top"] [
+        head [] [
+            meta [_charset "utf-8"]
+            meta [_name "viewport"; _content "width=device-width, initial-scale=1" ]
+            title [] [encodedText ("Tennis | " + dir)]
+            link [_rel "stylesheet"; _href "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" ]
+            link [_rel "stylesheet"; _href "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css" ]
+
+        ]
+        body [] [
+            yield nav [ _class "navbar is-fixed-top has-shadow" ] [
+                div [_class "navbar-brand"] [
+                    a [_class "navbar-item"; _href "/"] [
+                        img [_src "https://avatars0.githubusercontent.com/u/35305523?s=200"; _width "28"; _height "28"]
+                    ]
+                ]
+                div [_class "navbar-menu"; _id "navMenu"] [
+                    div [_class "navbar-start"] [
+                        span [_class "navbar-item";] [rawText ("Current path - " + dir)]
+                    ]
+                    div [_class "navbar-end"] [
+                        p [_class "navbar-item"] [
+                            rawText "Powered by "
+                            a [_href "https://github.com/SaturnFramework/Saturn"] [rawText " Saturn"]
+                            rawText " - F# MVC framework created by "
+                            a [_href "http://lambdafactory.io"] [rawText " λFactory"]
+                        ]
+                    ]
+                ]
+            ]
+            yield! content
+        ]
+]
+
+
+let fileList (runningDir: string) (currentDir: string) (dirs: string list) (files: string list) =
+    let s =
+        section [_class "section"] [
+            div [_class "content"] [
+                ul [] [
+                    yield! dirs |> List.map (fun n ->
+                        let p = Path.GetRelativePath(runningDir, n)
+                        let name = Path.GetRelativePath(currentDir, n)
+                        li [] [ a [_href p] [rawText name ]])
+                    yield! files |> List.map (fun n ->
+                        let p = Path.GetRelativePath(runningDir, n)
+                        let name = Path.GetRelativePath(currentDir, n)
+                        li [] [a [_href p] [rawText name ]] )
+                ]
+            ]
+        ]
+    layout currentDir [s]
---
+++ src/Program.fs
@@ -0,0 +1,37 @@
+﻿open Saturn
+open System
+open System.IO
+open Giraffe
+open Layout
+open System.IO
+
+let dir = Directory.GetCurrentDirectory()
+
+let handler = scope {
+    getf "%s" (fun s ->
+        let p = dir + s.Replace("/", "\\")
+        printfn "PATH: %s" p
+        if Directory.Exists p then
+            let dirs = Directory.GetDirectories p |> Seq.toList
+            let files = Directory.GetFiles(p, "*.*", SearchOption.TopDirectoryOnly) |> Seq.toList
+            printfn "DIRS: %A" dirs
+            printfn "FILES: %A" files
+            htmlView (fileList dir p dirs files)
+        elif File.Exists p then
+            streamFile true p None None
+        else
+            htmlView (layout p [])
+
+    )
+}
+
+let app port = application {
+    url (sprintf "http://0.0.0.0:%d/" port)
+    router handler
+}
+
+[<EntryPoint>]
+let main _ =
+    app 8080
+    |> run
+    0
